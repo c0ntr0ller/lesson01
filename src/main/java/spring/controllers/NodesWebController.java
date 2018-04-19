48981bd5383e3e1e83d4b2a488c77db113325ac4
c0ntr0ller <bek.andrey@gmail.com>
2018-04-19 23:31:17 +0700

    10 RangeGet

--- src/main/java/spring/controllers/NodesWebController.java
+++ src/main/java/spring/controllers/NodesWebController.java
@@ -3,10 +3,7 @@ package spring.controllers;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import spring.repository.RestLocalNodeRepository;
 
 import java.math.BigInteger;
@@ -25,6 +22,18 @@ public class NodesWebController {
         String result = null;
         try {
             result = mapper.writeValueAsString(restLocalNodeRepository.findOne(nodeId));
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @RequestMapping(value = "/dist", method = RequestMethod.GET)
+    String getNodesInRange(@RequestParam("lat") Double lat, @RequestParam("lon") Double lon,
+                           @RequestParam("dist") Double dist, @RequestParam(name = "lim", defaultValue = "100") Integer lim){
+        String result = "";
+        try {
+            result = mapper.writeValueAsString(restLocalNodeRepository.findNodesInRange(lat, lon, dist, lim));
         } catch (JsonProcessingException e) {
             e.printStackTrace();
         }
--- src/main/java/spring/repository/RestLocalNodeRepository.java
+++ src/main/java/spring/repository/RestLocalNodeRepository.java
@@ -1,9 +1,15 @@
 package spring.repository;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import spring.entities.LocalNode;
 
 import java.math.BigInteger;
+import java.util.List;
 
 public interface RestLocalNodeRepository extends PagingAndSortingRepository<LocalNode, BigInteger> {
+    @Query(value = "SELECT * FROM nodes WHERE earth_distance(ll_to_earth(:lat, :lon), ll_to_earth(lat, lon)) < :dist limit(:lim)",
+            nativeQuery = true)
+    List<LocalNode> findNodesInRange(@Param("lat") Double lat, @Param("lon") Double lon, @Param("dist") Double dist, @Param("lim") Integer lim);
 }
